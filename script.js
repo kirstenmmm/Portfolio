b4d2ad6bd51eeaa7c2643daf899593b37fb0dc36
kirstenmmm <kirstenmjeremicz10@gmail.com>
2024-12-03 11:37:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let navBar = document.querySelector(".nav-bar");
+
+function openMenu(){
+    console.log("open!");
+
+    navBar.style.width = "90%";
+    navBar.style.opacity = "1"; 
+}
+
+function closeMenu(){
+    navBar.style.width = "0%";
+    navBar.style.opacity = "0";
+}
+
+function returnHome(){
+}
